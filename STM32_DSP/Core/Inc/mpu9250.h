9b1408104b890c4d3c4f1519694ec9ea02183f95
Triet <76050658+trietmt9@users.noreply.github.com>
2024-02-04 17:38:01 +0700

    mpu9250 library

--- STM32_DSP/Core/Inc/mpu9250.h
+++ STM32_DSP/Core/Inc/mpu9250.h
@@ -7,7 +7,7 @@
 
 #ifndef INC_MPU9250_H_
 #define INC_MPU9250_H_
-
+#include <stdint.h>
 /** MPU9250 REGISTERS USED IN THIS PROJECT MACROS **/
 
 /* MACROS for initialize the IMU */
@@ -22,5 +22,32 @@
 /* MACROS for reading the IMU*/
 #define GYRO_OUT                                        0X43
 #define ACCEL_OUT                                       0X3B
+
+
+/** STRUCT FOR READING IMU DATA **/
+typedef struct 
+{
+/* RAW data read from sensor */
+    int16_t Gx_RAW;
+    int16_t Gy_RAW;
+    int16_t Gz_RAW; 
+    int16_t Ax_RAW;
+    int16_t Ay_RAW;
+    int16_t Az_RAW;
+
+/* Variables for calculations */
+
+    float Gx;
+    float Gy;
+    float Gz;
+    float Gx_Callib;
+    float Gy_Callib;
+    float Gz_Callib;
+    float Ax;
+    float Ay;
+    float Az;
+    float Roll;
+    float Pitch;
+}imu;
 
 #endif /* INC_MPU9250_H_ */
