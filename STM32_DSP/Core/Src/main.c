dd69909b1980075c794179a05286e6b1ba5f79df
Triet <76050658+trietmt9@users.noreply.github.com>
2024-02-27 21:34:16 +0700

    add I2C librảy for mpu9250

--- STM32_DSP/Core/Src/main.c
+++ STM32_DSP/Core/Src/main.c
@@ -42,6 +42,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
 SPI_HandleTypeDef hspi1;
 
 UART_HandleTypeDef huart2;
@@ -57,6 +59,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -82,7 +85,7 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-  IMU_Init(&MPU9250_t);
+
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -96,8 +99,9 @@ int main(void)
   MX_GPIO_Init();
   MX_SPI1_Init();
   MX_USART2_UART_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
-
+  IMU_Init(&MPU9250_t);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -105,13 +109,13 @@ int main(void)
   while (1)
   {
     IMU_Read(&MPU9250_t);
-    g_Ax = MPU9250_t.Data_t.Ax;
-    g_Ay = MPU9250_t.Data_t.Ay;
-    g_Az = MPU9250_t.Data_t.Az;
+    g_Ax = MPU9250_t.RawData_t.Ax_RAW;
+    g_Ay = MPU9250_t.RawData_t.Ay_RAW;
+    g_Az = MPU9250_t.RawData_t.Az_RAW;
 
-    sprintf(Ax, "%.2f\n", g_Ax);
-    HAL_UART_Transmit(&huart2,(uint8_t*) &Ax, strlen(Ax), 10);
-    HAL_Delay(500);
+    // sprintf(Ax, "%.2f\n", g_Ax);
+    // HAL_UART_Transmit(&huart2,(uint8_t*) &Ax, strlen(Ax), 100);
+    // HAL_Delay(500);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -158,6 +162,40 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
@@ -246,6 +284,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(IMU_CS_GPIO_Port, IMU_CS_Pin, GPIO_PIN_RESET);
